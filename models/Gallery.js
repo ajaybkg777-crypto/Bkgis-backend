678a89562b0bc833f304b0bac7ab3a934f394989
ajaybkg777-crypto <ajaybkg777@gmail.com>
2026-01-01 22:23:08 +0530

    Update Gallery.js

--- models/Gallery.js
+++ models/Gallery.js
@@ -2,33 +2,12 @@ const mongoose = require("mongoose");
 
 const GallerySchema = new mongoose.Schema(
   {
-    event: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-    category: {
-      type: String,
-      required: true,
-      enum: ["junior", "senior"],
-    },
-
-    // Cloudinary image URL
-    url: {
-      type: String,
-      required: true,
-    },
-
-    // Cloudinary public id (for delete / update)
-    public_id: {
-      type: String,
-      required: true,
-    },
+    event: String,
+    category: String,
+    url: String,
+    public_id: String,
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Gallery", GallerySchema);
