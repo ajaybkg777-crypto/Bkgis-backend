39ca037ce0421f7880f4ee686cc1195c81a87b05
ajaybkg777-crypto <ajaybkg777@gmail.com>
2026-02-19 19:41:08 +0530

    Update Gallery.js

--- models/Gallery.js
+++ models/Gallery.js
@@ -2,10 +2,40 @@ const mongoose = require("mongoose");
 
 const GallerySchema = new mongoose.Schema(
   {
-    event: String,
-    category: String,
-    url: String,
-    public_id: String,
+    event: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      enum: ["junior", "senior"],
+      required: true,
+    },
+
+    // photo | video
+    type: {
+      type: String,
+      enum: ["photo", "video"],
+      default: "photo",
+    },
+
+    // Photo URL OR Video Thumbnail URL
+    url: {
+      type: String,
+      required: true,
+    },
+
+    // Cloudinary public id
+    public_id: {
+      type: String,
+    },
+
+    // Only for video (YouTube)
+    videoLink: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
